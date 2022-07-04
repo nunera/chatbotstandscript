8880831046384011c6ff691f656f2a31d313e402
nunera <67439647+nunera@users.noreply.github.com>
2022-07-04 16:53:43 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
+--all lowercase and include the executor account too
+local allwhitelisted = false
+
 local whitelist = {
 	"jeronimoxfelinor",
-	"blatantdefiance"
-
+	"blatantdefiance",
+	"penguinkowaski"
 }
 local baseplate = false
 local bringphrase = "Penguin, analysis."
@@ -9,21 +12,7 @@ local dismissphrase = "Affirmative."
 local plrs = game:GetService("Players")
 local allplrs = plrs:GetChildren()
 
-local whitelistsingame = {
-	
-	
-	
-}
-
-
-
-for i,v in pairs(allplrs) do
-    local newplayer = string.lower(v.Name)
-	if table.find(whitelist,v) then
-		table.insert(whitelistsingame,newplayer)
-		print(newplayer,"is whitelisted!")
-	end
-end
+local whitelistsingame = {}
 
 print("Chat bot stand activated!")
 local HttpService = game:GetService("HttpService")
@@ -31,18 +20,18 @@ local player = game:GetService("Players").LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
 local humpart = char:WaitForChild("HumanoidRootPart")
 local tweenservice = game:GetService("TweenService")
-local tweeninfo = TweenInfo.new(1,Enum.EasingStyle.Sine,Enum.EasingDirection.InOut)
-local istweeninfo = TweenInfo.new(.01,Enum.EasingStyle.Sine,Enum.EasingDirection.InOut)
+local tweeninfo = TweenInfo.new(1, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut)
+local istweeninfo = TweenInfo.new(.01, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut)
 
 --EVENTUALLY WANT TO WELD INSTEAD OF CFRAME LOOP
 _G.is = false
 local partpos = Vector3.new(5.741, 150.286, 21.387)
 local personCFrame = CFrame.new(5.74100018, 160.2860031, 21.3869991, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 local plrs = game:GetService("Players")
-function getChat(message,plrname)
+function getChat(message, plrname)
 	local HttpService = game:GetService("HttpService")
 	local Split = message:gsub(" ", "+") -- This takes spaces within the person's message and turns it into for ex: Hi+How+Are+You... to ensure that there are no errors and to also make the URL work
-	local Response = game:HttpGet("https://api.affiliateplus.xyz/api/chatbot?message="..Split)
+	local Response = game:HttpGet("https://api.affiliateplus.xyz/api/chatbot?message=" .. Split)
 	local Data = HttpService:JSONDecode(Response)
 	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(Data.message, "All")
 end
@@ -50,14 +39,14 @@ local function tween(plr)
 	if _G.is then
 		while _G.is do
 			local whitelistcframe = plr.Character.HumanoidRootPart.CFrame
-			local offset = CFrame.new(3,0,3)
+			local offset = CFrame.new(3, 0, 3)
 			local newcframe = whitelistcframe:ToWorldSpace(offset)
-			local tween = tweenservice:Create(humpart,istweeninfo,{CFrame = newcframe})
+			local tween = tweenservice:Create(humpart, istweeninfo, {CFrame = newcframe})
 			tween:Play()
 			wait(.01)
 		end
 	else
-		local tween = tweenservice:Create(humpart,tweeninfo,{CFrame = personCFrame})
+		local tween = tweenservice:Create(humpart, tweeninfo, {CFrame = personCFrame})
 		tween:Play()
 	end
 end
@@ -66,39 +55,37 @@ if baseplate then
 	part.Parent = workspace
 	part.Anchored = true
 	part.Position = partpos
-	part.Size = Vector3.new(5,1,5)
+	part.Size = Vector3.new(5, 1, 5)
 	wait(2)
 	tween()
 end
-plrs.PlayerAdded:Connect(function(plr)
-	local list = string.lower(plr.Name)
-	plr.Chatted:Connect(function(mes)
-		if table.find(whitelist,list) then
-			if mes == bringphrase and not _G.is then
-				_G.is = true
-				coroutine.resume(coroutine.create(tween(plr)))
-			end
-			if _G.is then
-				if mes == dismissphrase then
-					_G.is = false
-					if baseplate then
-						tween()
+
+for i, plr in pairs(allplrs) do
+	local newplayer = string.lower(plr.Name)
+	if table.find(whitelist, newplayer) or allwhitelisted then
+		table.insert(whitelistsingame, newplayer)
+		print(newplayer, "is whitelisted!")
+		local list = string.lower(plr.Name)
+		plr.Chatted:Connect(
+			function(mes)
+				if table.find(whitelistsingame, newplayer) or allwhitelisted then
+					local sep = string.split(mes, " ")
+					if mes == bringphrase and not _G.is then
+						_G.is = true
+						coroutine.resume(coroutine.create(tween(plr)))
 					end
-				else
-					local sep = string.split(mes," ")
-					if sep[1] == "!add" then
-						local user = string.lower(sep[2])
-						table.insert(whitelist,user)
-					else
-						if sep[1] == "!remove" then
-							local user = string.lower(sep[2])
-							table.remove(whitelist,user)
+					if _G.is then
+						if mes == dismissphrase then
+							_G.is = false
+							if baseplate then
+								tween()
+							end
 						else
-							getChat(mes,plr.Name)
+							getChat(mes, plr.Name)
 						end
 					end
 				end
 			end
-		end
-	end)
-end)
+		)
+	end
+end
